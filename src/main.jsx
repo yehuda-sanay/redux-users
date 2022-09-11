e362f80888d619d10504cd1a1a65b93dd5752a27
yehuda-sanay <yehudasanay1@gmail.com>
2022-09-11 16:00:29 +0300

    11.09.22

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import './index.css'
 import {createStore ,compose} from 'redux'
 import { Provider } from 'react-redux'
 import usersReducer from '../src/stor/reducer/users_reducer'
+import allReducers from '../src/stor/reducer/index'
 const enhancers = compose(
   window._REDUX_DEVTOOLS_EXTENSION_ &&
  window._REDUX_DEVTOOLS_EXTENSION_(),
  )
 
-const userStor = createStore(usersReducer,{users:[{fName:"yehuda",lName:"sanay"}]},enhancers)
+const userStor = createStore(allReducers,{},enhancers)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={userStor}>
